d125ed58ccb80934f198961687acb6e15bda3f70
S Dinesh Ram <s.dineshram2004@gmail.com>
2026-06-04 01:19:38 +0530

    chore: delete commented-out boilerplate files

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,21 +1,2 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
-
-
-# class InventoryManagement(http.Controller):
-#     @http.route('/inventory_management/inventory_management', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/inventory_management/inventory_management/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('inventory_management.listing', {
-#             'root': '/inventory_management/inventory_management',
-#             'objects': http.request.env['inventory_management.inventory_management'].search([]),
-#         })
-
-#     @http.route('/inventory_management/inventory_management/objects/<model("inventory_management.inventory_management"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('inventory_management.object', {
-#             'object': obj
-#         })
+# HTTP controllers go here when online sales / API endpoints are added.
